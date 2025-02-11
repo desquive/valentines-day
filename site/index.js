eabc799f8b49a736995a737aa4f2f03a824fddf9
desquive <desquive@usc.edu>
2025-02-10 19:57:47 -0800

    made edits to index.js

--- site/index.js
+++ site/index.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={} />
-            </Routes>
-        </BrowserRouter>
+        <App/>
     </React.StrictMode>
 );
